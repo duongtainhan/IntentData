f8edd1a86ea18d91074892937bcad8b2c14a730f
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-07-24 14:30:52 +0700

    Init Intent in Main

--- app/src/main/java/com/example/duongtainhan555/intentdata/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/intentdata/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.duongtainhan555.intentdata;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnSendString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Anh xa
+        btnSendString = findViewById(R.id.btnSendString);
+
+        btnSendString.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                intent.putExtra("String","Hello Main 2!!!");
+                startActivity(intent);
+            }
+        });
     }
 }
