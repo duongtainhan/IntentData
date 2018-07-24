3672071fd2f68de12fb5b348f27ac5d77dfa7d82
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-07-24 14:39:00 +0700

    Get intent in Main 2

--- app/src/main/java/com/example/duongtainhan555/intentdata/Main2Activity.java
+++ app/src/main/java/com/example/duongtainhan555/intentdata/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.example.duongtainhan555.intentdata;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        if(intent!=null)
+        {
+            String chuoi = intent.getStringExtra("String");
+            Toast.makeText(this,chuoi,Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this,"No value in ",Toast.LENGTH_SHORT).show();
+        }
     }
 }
